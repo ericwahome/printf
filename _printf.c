b5cbf48cf970e3a58fcd028ee04c9fb536541bf9
ericwahome <ewahome68@yahoo.com>
2022-07-12 17:11:38 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -1,10 +1,44 @@
 #include "main.h"
 #include <stdarg.h>
+
+/**
+ *  * check_format - checks if there is a valid format specifier
+ *   * @format: possible valid format specifier
+ *    * Return: pointer to valid function or NULL
+ *     */
+int (*check_format(const char *format))(va_list)
+{
+		int i = 0;
+			print_t p[] = {
+						{"c", print_c},
+								{"s", print_s},
+										{"i", print_i},
+												{"d", print_d},
+														{"b", print_b},
+																{"u", print_u},
+																		{"o", print_o},
+																				{"x", print_x},
+																						{"X", print_X},
+																								{"p", print_p},
+																										{"S", print_S},
+																												{"r", print_r},
+																														{"R", print_R},
+																																{NULL, NULL}
+							};
+
+				for (; p[i].t != NULL; i++)
+						{
+									if (*(p[i].t) == *format)
+													break;
+										}
+					return (p[i].f);
+}
+
 /**
  *  * _printf - function for format printing
  *   * @format: list of arguments to printing
- *    * Return: Number of characters to printing
- *     */
+ *   Return: Number of characters to printing
+ *    *  */
 int _printf(const char *format, ...)
 {
 		va_list ap;
@@ -21,7 +55,7 @@ int _printf(const char *format, ...)
 															{
 																			_putchar(format[i]);
 																						counter++;
-																									i++;
+																						i++;
 																												continue;
 																														}
 														else
